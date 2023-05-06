63357d1c45c2b9d872ec629d454011b14c1aded9
wgmp <wesley.pf@outlook.com>
2023-05-05 22:34:58 -0300

    First app

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { collection, query, onSnapshot } from 'firebase/firestore'
+import db from './src/databse/firebase'
+
 
 export default function App() {
+  const [nome, setNome] = useState('Carregando...')
+  const [idade, setIdade] = useState('')
+
+
+  useEffect(() => {
+     db.on('value', snapshot =>{
+      let data = snapshot.val()
+      const items = Object.values(data);
+      setNome(items)
+     })
+
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      {
+        nome.length > 0 ?(
+          <Text>{nome}</Text>
+        ):
+        (
+          <Text>No item</Text>
+        )
+      }
+
     </View>
   );
 }
